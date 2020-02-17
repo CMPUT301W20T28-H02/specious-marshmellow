c2f5879844771c30c020d57ca0de13b5d2576696
jawniss <johnas@ualberta.ca>
2020-02-17 12:38:49 -0700

    Added map

--- code/app/src/main/java/com/project/speciousmarshmellow/Driver.java
+++ code/app/src/main/java/com/project/speciousmarshmellow/Driver.java
@@ -1,28 +1,28 @@
-public Driver extends User {
-  private double rating;
-
-  public Driver(String username, String password, String email, Wallet wallet, String phone, double rating, Location location, double rating) {
-    super(String username, String password, String email, Wallet wallet, String phone, Double rating, Location location);
-    this.rating = rating;
-  }
-
-  public void acceptRequest(Request request) {
-
-  }
-
-  public void getPaid(double amount) {
-
-  }
-
-  public void searchRequests() {
-
-  }
-
-  public double getRating(){
-    return rating;
-  }
-
-  public double setRating(double rating){
-    this.rating = rating;
-  }
-}
+//public Driver extends User {
+//  private double rating;
+//
+//  public Driver(String username, String password, String email, Wallet wallet, String phone, double rating, Location location, double rating) {
+//    super(String username, String password, String email, Wallet wallet, String phone, Double rating, Location location);
+//    this.rating = rating;
+//  }
+//
+//  public void acceptRequest(Request request) {
+//
+//  }
+//
+//  public void getPaid(double amount) {
+//
+//  }
+//
+//  public void searchRequests() {
+//
+//  }
+//
+//  public double getRating(){
+//    return rating;
+//  }
+//
+//  public double setRating(double rating){
+//    this.rating = rating;
+//  }
+//}
