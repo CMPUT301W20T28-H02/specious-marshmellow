bec7c0fd8f3d06437bcf9e52c841376ece4aa5a0
jawniss <johnas@ualberta.ca>
2020-02-17 11:30:10 -0700

    RideRequest skeleton code

---
+++ code/Johnas/RideRequest.java
@@ -0,0 +1,27 @@
+public class RideRequest 
+{
+    private Rider rider;
+    private Driver driver;
+    private Double fare;
+    private Location startLocation;
+    private Location endLocation;
+
+
+    RideRequest()
+    {
+        
+    }
+
+
+    public void getStatus()
+    {
+
+    }
+
+
+    public void setStatus()
+    {
+        
+    }
+
+}
