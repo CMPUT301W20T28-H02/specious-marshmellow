d7c59f7c331bc805a8b278377793dd25e9894d2a
Marcus Blair <mblair@ualberta.ca>
2020-02-17 11:56:38 -0700

    Added Wallet Class

---
+++ code/app/src/main/java/com/project/speciousmarshmellow/Wallet.java
@@ -0,0 +1,29 @@
+package com.project.speciousmarshmellow;
+
+public class Wallet {
+    private double balance;
+
+    public Wallet() {
+        this.balance = 0;
+    }
+
+    public double checkBalance() {
+        // return the amount in the user wallet
+        return this.balance;
+
+    }
+
+    public void depositQRBucks(QRCode qrBucks) {
+        // add the total generated from the qrBucks to the user balance
+        this.balance += qrBucks.getAmount();
+    }
+
+    public QRCode withdrawQRBucks(double amount) {
+        // decrease the user balance by the amount to withdraw
+        // generate qr code for the amount
+        this.balance -= amount;
+
+        QRCode qrCode = new QRCode();
+        return qrCode;
+    }
+}
