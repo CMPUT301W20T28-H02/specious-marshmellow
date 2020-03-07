7dad5f9d318fe70321ed5c7f54fae3f933c7fd4e
sirjanchawla <sirjan@ualberta.ca>
2020-03-07 13:01:08 -0700

    can_request button works, current location works

--- databasedemo/DatabaseDemo/app/src/main/java/com/example/databasedemo/currentRequest.java
+++ databasedemo/DatabaseDemo/app/src/main/java/com/example/databasedemo/currentRequest.java
@@ -1,28 +1,87 @@
 package com.example.databasedemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-public class currentRequest extends AppCompatActivity {
-//    private GoogleMap map;
-//    LatLng latLng;
-//    mapFragment = ( SupportMapFragment ) getSupportFragmentManager()
-//                .findFragmentById( R.id.map);
-//    mapFragment.getMapAsync((OnMapReadyCallback) this);
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+public class currentRequest extends FragmentActivity implements OnMapReadyCallback {
+     GoogleMap map;
+     LatLng latLng;
+     FusedLocationProviderClient fusedLocationProviderClient;
+     Button can_Request;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_request);
+
+        can_Request = findViewById(R.id.can_request);
+
+
+
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync((OnMapReadyCallback) this);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+
+        requestPermission();
+        if(ActivityCompat.checkSelfPermission(currentRequest.this, ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(currentRequest.this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if(location != null){
+                    latLng = new LatLng(location.getLatitude(),location.getLongitude());
+                    MarkerOptions p3 = new MarkerOptions().position(latLng);
+                    map.addMarker(p3);
+                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+
+                }
+
+            }
+        });
+        can_Request.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), RiderDriverInitialActivity.class);
+                intent.putExtra("driver", false);
+                startActivity(intent);
+
+
+            }
+        });
 
 //        String longitudeString = getIntent().getStringExtra("Longitude");
 //        String latitudeString = getIntent().getStringExtra("Latitude");
@@ -34,17 +93,24 @@ public class currentRequest extends AppCompatActivity {
 //        MarkerOptions p1 = new MarkerOptions().position(latLng);
 //        map.addMarker(p1);
 //        map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+
+
     }
 
 
 
-//    public void onMapReady(GoogleMap googleMap) {
-//        map = googleMap;
-//
-//        /*LatLng UofAQuad = new LatLng( 53.526891, -113.525914 ); // putting long lat of a pin
-//        map.addMarker( new MarkerOptions().position(UofAQuad).title("U of A Quad") );  // add a pin
-//        map.moveCamera(CameraUpdateFactory.newLatLng( UofAQuad ) ); // center camera around the pin*/
-//    }
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this,new String[]{ACCESS_FINE_LOCATION},1);
+    }
+
+
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        /*LatLng UofAQuad = new LatLng( 53.526891, -113.525914 ); // putting long lat of a pin
+        map.addMarker( new MarkerOptions().position(UofAQuad).title("U of A Quad") );  // add a pin
+        map.moveCamera(CameraUpdateFactory.newLatLng( UofAQuad ) ); // center camera around the pin*/
+    }
 
 
 }
