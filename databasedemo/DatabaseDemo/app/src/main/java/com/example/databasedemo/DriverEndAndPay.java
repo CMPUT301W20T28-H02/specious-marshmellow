077d33afadbd39de134292eed21afe7dae08e51b
=HusseinWarsame <=warsameh7@gmail.com>
2020-03-12 20:02:21 -0600

    added rider and driver payment activities

--- databasedemo/DatabaseDemo/app/src/main/java/com/example/databasedemo/DriverEndAndPay.java
+++ databasedemo/DatabaseDemo/app/src/main/java/com/example/databasedemo/DriverEndAndPay.java
@@ -1,11 +1,16 @@
 package com.example.databasedemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -15,12 +20,19 @@ public class DriverEndAndPay extends AppCompatActivity implements OnMapReadyCall
 
     GoogleMap map;
     Button driverEndAndPayButton;
+    // integer code to request camera permission
+    private static final int ZXING_CAMERA_PERMISSION = 1;
+
+    // used for creating intent
+    private Class<?> mClss;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_end_and_pay);
         driverEndAndPayButton = findViewById(R.id.driver_end_and_pay_button);
+
+        mClss = DriverScannerActivity.class;
 
         Intent i = getIntent();
         final String riderUsername = i.getStringExtra("riderUsername");
@@ -33,15 +45,29 @@ public class DriverEndAndPay extends AppCompatActivity implements OnMapReadyCall
         driverEndAndPayButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(getBaseContext(), DriverEndAndPay.class);
-                i.putExtra("riderUsername", riderUsername);
-                i.putExtra("driverUusername", driverUsername);
+//                Intent i = new Intent(getBaseContext(), DriverScannerActivity.class);
+//                i.putExtra("riderUsername", riderUsername);
+//                i.putExtra("driverUusername", driverUsername);
+
+                if (ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+                    ActivityCompat.requestPermissions( DriverEndAndPay.this,
+                            new String[]{Manifest.permission.CAMERA}, ZXING_CAMERA_PERMISSION);
+                } else {
+                    Intent intent = new Intent(getBaseContext(), mClss);
+                    intent.putExtra("riderUsername", riderUsername);
+                    intent.putExtra("driverUsername", driverUsername);
+                    startActivity(intent);
+                }
 
             }
         });
 
 
     }
+
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -50,5 +76,21 @@ public class DriverEndAndPay extends AppCompatActivity implements OnMapReadyCall
         /*LatLng UofAQuad = new LatLng( 53.526891, -113.525914 ); // putting long lat of a pin
         map.addMarker( new MarkerOptions().position(UofAQuad).title("U of A Quad") );  // add a pin
         map.moveCamera(CameraUpdateFactory.newLatLng( UofAQuad ) ); // center camera around the pin*/
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,  String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case ZXING_CAMERA_PERMISSION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if(mClss != null) {
+                        Intent intent = new Intent(this, mClss);
+                        startActivity(intent);
+                    }
+                } else {
+                    Toast.makeText(this, "Please grant camera permission to use the QR Scanner", Toast.LENGTH_SHORT).show();
+                }
+                return;
+        }
     }
 }
--- databasedemo/DatabaseDemo/app/src/main/java/com/example/databasedemo/RiderEndAndPay.java
+++ databasedemo/DatabaseDemo/app/src/main/java/com/example/databasedemo/RiderEndAndPay.java
@@ -9,25 +9,48 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class RiderEndAndPay extends AppCompatActivity {
-    Button endRideButton;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class RiderEndAndPay extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
+    Button riderEndAndPayButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rider_end_and_pay);
-        endRideButton = findViewById(R.id.end_ride_button);
+        riderEndAndPayButton = findViewById(R.id.rider_end_and_pay_button);
 
-        endRideButton.setOnClickListener(new View.OnClickListener() {
+        Intent i = getIntent();
+        final String riderUsername = i.getStringExtra("riderUsername");
+        final String driverUsername = i.getStringExtra("driverusername");
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.driver_ride_map);
+        mapFragment.getMapAsync(this);
+
+        riderEndAndPayButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getBaseContext(), GenerateQR.class);
-                startActivity(i);
+            public void onClick(View view) {
+                Intent i = new Intent(getBaseContext(), RiderEndAndPay.class);
+                i.putExtra("riderUsername", riderUsername);
+                i.putExtra("driverUusername", driverUsername);
+
             }
         });
+
+
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
 
-
-
+        /*LatLng UofAQuad = new LatLng( 53.526891, -113.525914 ); // putting long lat of a pin
+        map.addMarker( new MarkerOptions().position(UofAQuad).title("U of A Quad") );  // add a pin
+        map.moveCamera(CameraUpdateFactory.newLatLng( UofAQuad ) ); // center camera around the pin*/
+    }
 }
