8aaeaf71d626e9ba134cbd9dbe9dd000ea295e37
jawniss <johnas@ualberta.ca>
2020-03-21 20:03:22 -0600

    Finally implemented pictures, so many Firebase storage and database errors

---
+++ android-code/DatabaseDemo/app/src/main/java/com/example/databasedemo/ProfilePictureFromFirebaseStorage.java
@@ -0,0 +1,99 @@
+package com.example.databasedemo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class ProfilePictureFromFirebaseStorage extends AppCompatActivity {
+
+    ImageView firebaseImage;
+    DatabaseReference reff;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_picture_from_firebase_storage);
+
+        firebaseImage = findViewById( R.id.firebase_image );
+
+        reff = FirebaseDatabase.getInstance().getReference().child("Profile pictures").child("Will_be_username");
+        // here gonna have to adjust reff to accurately go to the correct
+        // user, so i think add an if statement
+        // at dataSnapshot.child("//username").getValue().toString();
+
+        reff.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String url = dataSnapshot.child("imageUrl").getValue().toString();
+
+                Log.d("Firebase", url);
+
+                Picasso.get()
+                        .load( url )
+                        .into( firebaseImage );
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+    }
+
+
+
+    public static Drawable LoadImageFromWebOperations(String url) {
+        try {
+            InputStream is = (InputStream) new URL(url).getContent();
+            Drawable d = Drawable.createFromStream(is, "src name");
+            return d;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+
+    private Bitmap getImageBitmap(String url) {
+        Bitmap bm = null;
+        try {
+            URL aURL = new URL(url);
+            URLConnection conn = aURL.openConnection();
+            conn.connect();
+            InputStream is = conn.getInputStream();
+            BufferedInputStream bis = new BufferedInputStream(is);
+            bm = BitmapFactory.decodeStream(bis);
+            bis.close();
+            is.close();
+        } catch (IOException e) {
+            Log.e("Uri bitmap: ", "Error getting bitmap", e);
+        }
+        return bm;
+    }
+
+
+}
+
