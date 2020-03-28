460df30d0b3e92ee2464751b9f8c03c57d2963a5
Micheal Antifaoff <mantifao@ualberta.ca>
2020-03-28 01:14:19 -0600

    Add blue dot to DriverConfirmActivity

--- android-code/DatabaseDemo/app/src/main/java/com/example/databasedemo/DriverConfirmActivity.java
+++ android-code/DatabaseDemo/app/src/main/java/com/example/databasedemo/DriverConfirmActivity.java
@@ -211,7 +211,7 @@ public class DriverConfirmActivity extends AppCompatActivity implements OnMapRea
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
-
+        map.setMyLocationEnabled(true);
         /*LatLng UofAQuad = new LatLng( 53.526891, -113.525914 ); // putting long lat of a pin
         map.addMarker( new MarkerOptions().position(UofAQuad).title("U of A Quad") );  // add a pin
         map.moveCamera(CameraUpdateFactory.newLatLng( UofAQuad ) ); // center camera around the pin*/
